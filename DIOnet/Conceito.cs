196aae1292ed385821053f8165d8d784ce7598ee
patri <patri@DESKTOP-OA0GA2H>
2021-09-26 01:44:25 -0300

    Adicionar arquivos de projeto.

---
+++ DIOnet/Conceito.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIOnet
+{
+    public enum Conceito
+    {
+        A,
+        B,
+        C,
+        D,
+        E
+    }
+}
---
+++ DIOnet/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace DIOnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
+            string opcaoUsuario = ObterOpcaoUsuario();
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o nome do Aluno:");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do Aluno");
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("O Valor da Nota deve ser Decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+                        break;
+                    case "2":
+                        foreach(var a in alunos)
+                        {
+                            if(a.Nome !=null)
+                            { 
+                            Console.WriteLine($"Aluno: {a.Nome} - NOTA: {a.Nota}");
+                            }
+                        }
+                        break;
+                    case "3":
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0; //Numero Alunos
+                        for(int i=0; i< alunos.Length; i++)
+                        {
+                            if (alunos[i].Nome != null)
+                            {
+                                notaTotal = notaTotal + alunos[i].Nota;
+                                nrAlunos++;
+                            }
+                        }
+
+                        var mediaGeral = notaTotal / nrAlunos;
+                        Conceito conceitoGeral;
+                        if(mediaGeral < 2)
+                        {
+                            conceitoGeral = Conceito.E;
+                        }
+                        else if(mediaGeral < 4){
+                            conceitoGeral = Conceito.D;
+                        }
+                        else if (mediaGeral < 6) 
+                        {
+                            conceitoGeral = Conceito.C;
+                        }
+                        else if (mediaGeral < 8)
+                        {
+                            conceitoGeral = Conceito.B;
+                        }
+                        else
+                        {
+                            conceitoGeral = Conceito.A;
+                        }
+                        Console.WriteLine($"Média Geral: {mediaGeral} - Conceito: {conceitoGeral}");
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe uma Opção Desejada:");
+            Console.WriteLine("1- Inserir novo Aluno");
+            Console.WriteLine("2- Listar Alunos");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
